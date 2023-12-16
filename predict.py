4166480651e5a9a965f3c6cc9d800bc231329cc7
Denis Kalbasov <denis.kalbasov@gmail.com>
2023-12-16 19:10:06 +0300

    ready

--- predict.py
+++ predict.py
@@ -18,7 +18,7 @@ class Predictor(BasePredictor):
         self.face_swapper = insightface.model_zoo.get_model('cache/inswapper_128.onnx', providers=['CUDAExecutionProvider'])
         self.face_enhancer = gfpgan.GFPGANer(model_path='cache/GFPGANv1.4.pth', upscale=1)
         self.face_analyser = FaceAnalysis(name='buffalo_l')
-        self.face_analyser.prepare(ctx_id=0, det_size=(640, 640))
+        self.face_analyser.prepare(ctx_id=0, det_size=(512, 512))
 
     def get_face(self, img_data):
         analysed = self.face_analyser.get(img_data)
